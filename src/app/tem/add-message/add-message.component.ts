8ee37fae5fe2355d5df576fee825f6bf3914baf4
Emma Ramos <20001039@alumnos.utleon.edu.mx>
2024-11-08 18:38:28 -0600

    lista alumnos 08/11/2024

--- src/app/tem/add-message/add-message.component.ts
+++ src/app/tem/add-message/add-message.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageserviceService } from '../messageservice.service';
-import { FormBuilder, FormControl, FormGroup, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-message',
@@ -13,16 +13,14 @@ export class AddMessageComponent implements OnInit{
 
   formGroup!:FormGroup;
 
-  constructor(public messageservice: MessageserviceService, private fb: FormBuilder){}
+  constructor(public messageService: MessageserviceService, private fb: FormBuilder){}
   alumno: string=""
 
 ngOnInit(): void{
       this.formGroup=this.initForm();
 }
-  initForm(): FormGroup<any> {
-    throw new Error('Method not implemented.');
-  }
-  NgForm(): FormGroup  {
+
+  initForm(): FormGroup  {
       return this.fb.group({
         nombre:['']
    } )
@@ -30,7 +28,7 @@ ngOnInit(): void{
 
   addAlumno(){
     let {nombre}=this.formGroup.value;
-    this.messageservice.add(nombre)
+    this.messageService.add(nombre)
     this.formGroup.get('nombre')?.setValue('')  }
 
 }
